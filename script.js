6d477a7742d363d01d3f172145d194e627961a64
okhamar <okhamar@llnw.com>
2021-07-14 15:08:03 +0300

    minor fixes

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 function calcPercentage(){
   var x, y, tips;
-  x = document.getElementById('check').Value;
-  y = document.getElementById('percentage').Value;
-  tips = document.getElementById('tips').Value;
-}
-if ( (x < 0) || (y < 0) ){
-   document.getElementById('jsNegativeValue', 'jsNegativeValue2').innerHTML = '<p>*value is less than zero!"</p>';
-} else if (typeof (isNaN ( x )) || typeof (isNaN ( y )) ) {
-    document.getElementById('jsNegativeValue', 'jsNegativeValue2').innerHTML = '<p>*please enter a number not a text</p>';
-} else if ( (typeof (x) == undefined ) || (typeof (y) == undefined )) {
-    document.getElementById('jsNegativeValue', 'jsNegativeValue2').innerHTML = '<p>*please enter the value</p>';
-} else {
-    // document.getElementById("check").innerHTML = "Check number: " + x +"$";
-    // document.getElementById("percentage").innerHTML = "Tip: " + y + "%";
-    // tips = (x*y)/100;
-    // document.getElementById("tips").innerHTML = "Tip amount: " + tips +"$";
-    // res = Number(tips) + Number(x);
-    // document.getElementById("sum").innerHTML = "Total num to pay: " + res +"$";
-}
+  x = document.getElementById('check').value;
+  y = document.getElementById('percentage').value;
 
+if ((x == "") || (y == "")){
+    alert("Please enter the value!");
+} else if ((isNaN ( x )) || (isNaN ( y )) ) {
+    alert("Please enter a number not a text");
+} else if ( (x < 0 || x > 1000000) || (y < 0 || y > 100)) {
+    alert("Please input correct values. Check can not be greater than 1 000 000. Percentage should be less then 100%!");
+
+} else {
+     document.getElementById("check_number").innerHTML = x +"$";
+     document.getElementById("tip").innerHTML = "Tip: " + y + "%";
+     tips = (x*y)/100;
+     document.getElementById("tip_amount").innerHTML = "Tip amount: " + tips +"$";
+     res = Number(tips) + Number(x);
+     document.getElementById("total_sum").innerHTML = "Total sum to pay: " + res +"$";
+}
 }
